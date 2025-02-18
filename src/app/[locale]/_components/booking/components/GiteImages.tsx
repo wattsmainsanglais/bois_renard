8cf679cb80191fc46d69a4f8aa5770604b78a7bb
wattsmainsanglais <wattsmainsanglais@gmail.com>
2025-02-18 12:38:42 +0100

    added dialog modal for viewing images

--- src/app/[locale]/_components/booking/components/GiteImages.tsx
+++ src/app/[locale]/_components/booking/components/GiteImages.tsx
@@ -1,7 +1,14 @@
 import { StaticImageData } from "next/image";
 
+import {
+    Dialog,
+    DialogContent,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+
 import  Image from "next/image";
 import { Images } from "../Gite";
+import { DialogDescription, DialogTitle } from "@radix-ui/react-dialog";
 
 
 
@@ -11,14 +18,33 @@ export default function GiteImages({images}: {images: Images }){
     return (
         <div className="flex">
             {images.imageSrc.map((i, index)=> (
-                <Image 
-                    src={i}
-                    alt='gite image'
-                    width={128}
-                    height={128}
-                    key={index}
+            <Dialog>
+                <DialogTrigger asChild>
+                    <Image 
+                        src={i}
+                        alt='gite image'
+                        width={128}
+                        height={128}
+                        key={index}
 
-                />
+                    />
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-md">
+                    <DialogTitle aria-description="gite photo">
+
+                    </DialogTitle>
+                    <DialogDescription>
+                        <Image 
+                                src={i}
+                                alt='gite image'
+                                width={960}
+                                height={960}
+                                key={index}
+
+                            />
+                    </DialogDescription>
+                </DialogContent>
+            </Dialog>
             ))}
         </div>
 

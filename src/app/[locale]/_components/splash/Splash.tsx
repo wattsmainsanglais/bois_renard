3d7cae4f8e8f94e32a793e2165db2f1257f121a6
wattsmainsanglais <wattsmainsanglais@gmail.com>
2025-03-31 14:36:23 +0200

    chqnge syntax for css url() method - vercel doesn't like ''

--- src/app/[locale]/_components/splash/Splash.tsx
+++ src/app/[locale]/_components/splash/Splash.tsx
@@ -8,7 +8,7 @@ export default function Splash({locale}: {locale: string}){
 /* bg-[url('/Gite_Bois_Renard_Photo_FB.jpg')] */
 
     return(
-        <div className="w-screen h-screen bg-cover bg-center flex items-start justify-center" style={{backgroundImage: "url('/Gite_Bois_Renard_Photo_FB.jpg')"}}>
+        <div className="w-screen h-screen bg-cover bg-center flex items-start justify-center" style={{backgroundImage: "url(/Gite_Bois_Renard_Photo_FB.jpg)"}}>
             <NavBar locale={locale} /> 
            {/* <Image
                 src={splash}

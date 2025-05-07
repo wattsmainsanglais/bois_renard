0dadbe962fd9a42e8c8bc16d0be05beac11eef76
wattsmainsanglais <wattsmainsanglais@gmail.com>
2025-05-07 09:54:08 +0200

    move favicon files, add further meta tags

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -10,8 +10,8 @@ import Footer from "./_components/footer/Footer";
 
 
 export const metadata: Metadata = {
-  title: "La Gite du Bois Renard",
-  description: "Gite Deux Sevres | Gite South West France | Gite Sud Ouest France | Gite holiday | Gite Vacances",
+  title: "La Gite du Bois Renard - Deux Sevres",
+  description: "Gites Deux Sevres | Gites South West France | Gite Sud Ouest France | Gite holiday | Gite Vacances",
 };
 
 export type LayoutProps = {
@@ -33,6 +33,12 @@ export default async function RootLayout({
   return (
     <html lang={locale}>
       <head>
+        <meta name="keywords" content="Gite Deux Sevres, Gites South West France, Gites Sud Ouest France, Gites holiday, Gites Vacances " />
+        <meta name="description" content="Situated in Nouvelle Aquitaine in southwestern France, welcome to gites de bois renard, a traditional charentaise farmhouse built in the 1840’s in the rural village of Lorigne. Our little hamlet, Bois Renard, is located between the market towns of Sauze Vaussais and Chef Boutonne, surrounded by fields and woodland, and five minutes’ drive to all your daily needs - supermarket, pharmacies, restaurants, bars and bakeries." />
+        <meta property="og:title" content="La Gite du Bois Renard - Deux Sevres" />
+        <meta property="og:description" content="Situated in Nouvelle Aquitaine in southwestern France, welcome to gites de bois renard, a traditional charentaise farmhouse built in the 1840’s" />
+        <meta property="og:image" content="https://www.giteduboisrenard.fr/Gite_Bois_Renard_Photo_FB.jpg" />
+        
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link
           rel="apple-touch-icon"
@@ -40,6 +46,8 @@ export default async function RootLayout({
           type="image/<generated>"
           sizes="<generated>"
         />
+
+
 
       </head>
       <body>
